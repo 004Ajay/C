7ef49aaadb1e670a750e95f5280125fbe82a9827
004Ajay <85185407+004Ajay@users.noreply.github.com>
2021-12-07 19:06:08 +0530

    Adding two numbers in C

---
+++ AddTwoNumbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int num1, num2;
+    printf("Enter two Numbers: ");
+    scanf("%d %d", &num1, &num2); //getting numbers
+
+    printf("%d + %d = %d", num1, num2, num1+num2); //adding numbers and output
+    return 0;
+}
