515c86ebbd9691b989c84e82267a4eba167ac823
004Ajay <85185407+004Ajay@users.noreply.github.com>
2021-12-07 22:41:40 +0530

    ASCII Value of Chars

---
+++ ASCIIValueOfCharcter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    
+    printf("ASCII value of %c = %d", c, c);
+    // %d displays the integer value of entered character
+    // %c for actual character
+    return 0;
+}
